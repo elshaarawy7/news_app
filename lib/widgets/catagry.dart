f1f0b40752a7463fecfecf359e3cc88c9faeb0a8
ELshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-01-30 09:46:06 +0200

    create object imag and tetx and ontap

--- lib/widgets/catagry.dart
+++ lib/widgets/catagry.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 
 class CatagryCard extends StatelessWidget {
-  const CatagryCard({super.key});
+  const CatagryCard({
+    required this.imag ,
+    required this.text ,
+    required this.onTap ,
+     });
    
-
+   final String text ;
+   final String imag;
+   final void Function()? onTap;
   @override
   Widget build(BuildContext context) {
-    return GestureDetector();
+    return GestureDetector(
+      onTap: onTap,
+    );
   }
 }
