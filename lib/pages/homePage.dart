bdd9046cdcd3874ddf58e77c3fba5ac9545034a7
ELshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-01-30 09:36:41 +0200

    create appBar

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -6,7 +6,26 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      
+      appBar: AppBar(
+        title: const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("News" , style: TextStyle(
+              color: Colors.black ,
+              fontSize: 20 ,
+              fontWeight: FontWeight.bold ,
+            ),)  ,
+
+            SizedBox(width: 5,),
+
+            Text("App" , style: TextStyle(
+              color: Colors.amber ,
+              fontSize: 20 ,
+              fontWeight: FontWeight.bold ,
+            ),)
+          ],
+        ),
+      ),
     );
   }
 }
