9de65326ee365a8f5179a6b5471e4320fa21498d
ELshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-01-30 09:43:34 +0200

    create CatagryCard

--- lib/widgets/catagry.dart
+++ lib/widgets/catagry.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 
 class CatagryCard extends StatelessWidget {
   const CatagryCard({super.key});
+   
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return GestureDetector();
   }
 }
